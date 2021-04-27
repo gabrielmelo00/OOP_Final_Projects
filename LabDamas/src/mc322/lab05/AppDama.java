cda8d183ab8e8ab113ee55a7b2f68f9424f7ad60
Gabriel <gamvs1301@gmail.com>
2021-04-27 00:32:29 -0300

    paste

---
+++ LabDamas/src/mc322/lab05/AppDama.java
@@ -0,0 +1,11 @@
+package mc322.lab05;
+
+public class AppDama {
+
+	public static void main(String[] args) {
+		Tabuleiro meuTabuleiro = new Tabuleiro();
+		System.out.println(meuTabuleiro.retornaEstado());
+		
+	}
+
+}
