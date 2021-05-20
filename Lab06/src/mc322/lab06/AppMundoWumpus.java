de872e5e4c0c6d10079cb33b1bd9a8ace4e14c7f
Hannah Plath <plath.hannah@gmail.com>
2021-05-20 09:51:31 -0300

    20/05

--- Lab06/src/mc322/lab06/AppMundoWumpus.java
+++ Lab06/src/mc322/lab06/AppMundoWumpus.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class AppMundoWumpus {
 
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		String [][] tabuleiro;
 		CSVHandling csv = new CSVHandling();
-		csv.setDataSource("../Lab06/data/caverna2.csv");
+		csv.setDataSource(args[0]);
 		tabuleiro = csv.requestCommands();
 		
 		Montador meuMontador = new Montador(tabuleiro);
