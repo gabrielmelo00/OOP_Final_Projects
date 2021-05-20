a402de228bb3d030a19283d355b8c9557e83d34c
Hannah Plath <plath.hannah@gmail.com>
2021-05-20 10:28:42 -0300

    20/05

--- Lab06/src/mc322/lab06/AppMundoWumpus.java
+++ Lab06/src/mc322/lab06/AppMundoWumpus.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class AppMundoWumpus {
 
 	public static void main(String [] args) {
+		
 		String [][] tabuleiro;
 		CSVHandling csv = new CSVHandling();
 		csv.setDataSource(args[0]);
